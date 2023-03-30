fd671ea58d2ebc722789e1b89cc57b9a8fd2524b
dark-en-light <d-kudashkina@mail.ru>
2023-03-30 18:27:06 +0400

    Реализация программы по поиску информации по введённому номеру машины. По введённому номеру программа выдаёт информацию о категории ТС и субъекту РФ.

---
+++ src/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        NumberInfo numberInfo = new NumberInfo(); //Объявление класса "NumberInfo".
+        numberInfo.getNumberInfo(); //Вызов метода "getNumberInfo()" из класса "NumberInfo".
+    }
+}
